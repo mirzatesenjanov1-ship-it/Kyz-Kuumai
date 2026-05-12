937617ee2fbbee6167a4022db42a40d68ffaf525
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-05-12 20:38:16 +0600

    Change GEMINI_API_KEY in config.js
    Updated the GEMINI_API_KEY in the configuration.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
     // image_d2fef7.png файлындагы жаңы ачкыч
-    GEMINI_API_KEY: "AIzaSyBypyHe-IRhRksv1VMfUT2ZNIGn3IyvPRk"
+    GEMINI_API_KEY: "AIzaSyBMXNZDfigiZTxm6AQ3braW3G6pcKoYSyQ"
 };
