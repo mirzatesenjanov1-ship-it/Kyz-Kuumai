00a6c19069516f2b1bad88e1cd9a99d914bb616f
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-05-12 20:58:20 +0600

    Update GEMINI_API_KEY in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
     // image_d2fef7.png файлындагы жаңы ачкыч
-    GEMINI_API_KEY: "AIzaSyBMXNZDfigiZTxm6AQ3braW3G6pcKoYSyQ"
+    GEMINI_API_KEY: "AIzaSyC722kNLT6LulyGUdaYVrAuzOZj1S0DLSM"
 };
